d36507776d76912e2d061e2b7936598c53269a61
FireMMDC <mdelcampo2013@fau.edu>
2016-10-24 19:36:25 -0400

    added bootstrap-slider lib

---
+++ Homework/Homework 2/exercise page/js/sliderstart.js
@@ -0,0 +1,20 @@
+$('#slider-1').slider({
+	formatter: function(value) {
+		return 'Val:' + value;
+	}
+});
+$('#slider-2').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
+$('#slider-3').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
+$('#slider-4').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
