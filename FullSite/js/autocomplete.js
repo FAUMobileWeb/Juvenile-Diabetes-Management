f17a4c64114e5640a18a7723620ca5992968e587
FireMMDC <mdelcampo2013@fau.edu>
2016-11-29 00:06:42 -0500

    changed to twitter's typeahead and updated css for it to fit better

--- FullSite/js/autocomplete.js
+++ FullSite/js/autocomplete.js
@@ -1,2 +1,19 @@
-$('.typeahead').typeahead();
- $("#foodAuto").typeahead({ source:["a","abc","ab","abcd"] });
+var fruit = ['Apple', 'Bannna', 'Bread'
+];
+// constructs the suggestion engine
+var fruit = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // `states` is an array of state names defined in "The Basics"
+  local: fruit
+});
+
+$('#bloodhound .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'fruits',
+  source: fruit
+});
