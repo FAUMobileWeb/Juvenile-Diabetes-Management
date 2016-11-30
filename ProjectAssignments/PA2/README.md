fa1bbdb2bb02fd124b1cb449da840dd12fbac2ea
FireMMDC <mdelcampo2013@fau.edu>
2016-11-30 09:47:52 -0500

    moved current full side to  PA2

---
+++ ProjectAssignments/PA2/README.md
@@ -0,0 +1,4 @@
+#Project Assignment 2
+A progress on our Juvenile Diabetes Management website.
+
+Illustrates most of the basic webpages being used.
---
+++ ProjectAssignments/PA2/js/sliderstart.js
@@ -0,0 +1,20 @@
+var slider1 = $('#slider-1').slider({
+	formatter: function(value) {
+		return 'Val:' + value;
+	}
+});
+var slider2 = $('#slider-2').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
+var slider3 = $('#slider-3').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
+var slider4 = $('#slider-4').slider({
+	formatter: function(value) {
+		return 'Val: ' + value;
+	}
+});
