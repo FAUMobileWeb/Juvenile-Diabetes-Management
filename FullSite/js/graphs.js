a7d465d4159e0120549287eb3a95852c3a1b0b5b
FireMMDC <mdelcampo2013@fau.edu>
2016-12-07 12:45:52 -0500

    added both graphs for exercise

--- FullSite/js/graphs.js
+++ FullSite/js/graphs.js
@@ -95,6 +95,65 @@ $("#insulin").click(function () {
 
 });
 $("#exercise").click(function () {
+    var user = JSON.parse(localStorage.getItem(sessionStorage.getItem("name")));
+    if (user === null) {
+        window.location.href = "login.html";
+    } else {
+        var label = [];
+        var dataset = [];
+        var cals = user.exerciseWeight;
+        var orderedCals = {};
+        Object.keys(cals).sort().forEach(function (key) {
+            orderedCals[key] = cals[key];
+        })
+        $.each(orderedCals, function (key, value) {
+            label.push(value.date);
+            dataset.push(((value.pounds * value.rep) * value.set));
+        });
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: label,
+                datasets: [{
+                        label: "Total Weight Lifted",
+                        data: dataset
+                    }
+
+                    ]
+            }
+        });
+    }
+
+});
+$("#exerciseRun").click(function () {
+    var user = JSON.parse(localStorage.getItem(sessionStorage.getItem("name")));
+    if (user === null) {
+        window.location.href = "login.html";
+    } else {
+        var label = [];
+        var dataset = [];
+        var cals = user.exerciseCardio;
+        var orderedCals = {};
+        Object.keys(cals).sort().forEach(function (key) {
+            orderedCals[key] = cals[key];
+        })
+        $.each(orderedCals, function (key, value) {
+            label.push(value.date);
+            dataset.push(value.time);
+        });
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: label,
+                datasets: [{
+                        label: "Running in Minutes",
+                        data: dataset
+                    }
+
+                    ]
+            }
+        });
+    }
 
 });
 $("#weight").click(function () {
